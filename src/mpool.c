119eaf2ac9df9d0a9e510201e040666fa1d4ecb3
Abhishek Chakravarti <abhishek@taranjali.org>
2019-09-06 08:55:22 +0530

    incorporate error checking in mpool implementation
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/mpool.c
+++ src/mpool.c
@@ -9,13 +9,17 @@ extern rush_erno
 rush_mpool_alloc(void **bfr, size_t sz)
 {
 RUSH_TRY:
+        rush_assert_handle(bfr);
+        rush_assert_range(sz);
+
         if (!gc_init)
                 GC_INIT();
 
         *bfr = GC_MALLOC(sz);
-        rush_assert(bfr, RUSH_ERNO_MPOOL);
+        rush_assert_handle(*bfr);
 
 RUSH_CATCH:
+        rush_erno_set(RUSH_ERNO_MPOOL);
 
 RUSH_FINALLY:
         return rush_erno_get();
@@ -25,10 +29,20 @@ RUSH_FINALLY:
 extern rush_erno
 rush_mpool_realloc(void **bfr, size_t sz)
 {
+RUSH_TRY:
+        rush_assert_handle(bfr);
+        rush_assert_range(sz);
+
         if (!gc_init)
                 GC_INIT();
 
         *bfr = GC_REALLOC(*bfr, sz);
-        return *bfr != 0;
+        rush_assert_handle(*bfr);
+
+RUSH_CATCH:
+        rush_erno_set(RUSH_ERNO_MPOOL);
+
+RUSH_FINALLY:
+        return rush_erno_get();
 }
 
