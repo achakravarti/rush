042bc18de65deb7b9e13e8b99cba3ee2b44f89ad
Abhishek Chakravarti <abhishek@taranjali.org>
2019-09-06 12:00:56 +0530

    Add error handling module
    Error handling is an essential part of a good API, and the RμSH Library
    is no exception. An earlier experiment had been made to incorporate
    exception handling with the use of setjmp() and longjmp(), but this did
    not scale well. As a consequence, we're reverting to using the
    traditional approach of returning error codes from functions, with
    additional macros to serve as syntactic sugar.
    
    The error handling module has been defined with the following interface:
      - `rush_erno` type
      - `RUSH_ERNO_NUL` symbolic constant
      - `RUSH_ERNO_HANDLE` symbolic constant
      - `RUSH_ERNO_MPOOL` symbolic constant
      - `RUSH_ERNO_RANGE` symbolic constant
      - `RUSH_TRY` label
      - `RUSH_CATCH` label
      - `RUSH_FINALLY` label
      - `rush_erno_get()` macro
      - `rush_erno_set()` macro
      - `rush_try()` macro
      - `rush_assert()` macro
      - `rush_assert_handle()` macro
      - `rush_assert_range()` macro
    
    The `rush_mpool` implementation has also been refactored to make use of
    the newly incorporated error handling module. Testing and documentation
    are still pending.
    
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/api.h
+++ src/api.h
@@ -10,6 +10,55 @@
 
 typedef int rush_erno;
 #define RUSH_ERNO_NIL ((rush_erno) 0x0)
+#define RUSH_ERNO_HANDLE ((rush_erno) 0x1)
+#define RUSH_ERNO_MPOOL ((rush_erno) 0x2)
+#define RUSH_ERNO_RANGE ((rush_erno) 0x3)
+
+
+#define rush_erno_get() \
+        ((const rush_erno) rush__erno__)
+
+
+#define rush_erno_set(e) \
+        (rush__erno__ = (e))
+
+
+#define RUSH_TRY                                         \
+        register rush_erno rush__erno__ = RUSH_ERNO_NIL; \
+        goto RUSH__TRY__;                                \
+        RUSH__TRY__
+
+
+#define RUSH_CATCH            \
+        goto RUSH__FINALLY__; \
+        RUSH__CATCH__
+
+
+#define RUSH_FINALLY RUSH__FINALLY__
+
+
+#define rush_assert(p, e) \
+do {                                \
+        if (!(p)) {                 \
+                rush__erno__ = (e); \
+                goto RUSH__CATCH__; \
+        }                           \
+} while (0)
+
+
+#define rush_assert_handle(p) \
+        rush_assert((p), RUSH_ERNO_HANDLE)
+
+
+#define rush_assert_range(p) \
+        rush_assert((p), RUSH_ERNO_RANGE)
+
+
+#define rush_try(p)                 \
+do {                                \
+        if ((rush__erno__ = (p)))   \
+                goto RUSH__CATCH__; \
+} while (0)
 
 
 extern rush_erno
--- src/mpool.c
+++ src/mpool.c
@@ -2,27 +2,53 @@
 #include "api.h"
 
 
-static thread_local bool gc_init = false;
+static thread_local bool gc_flag = false;
+
+
+static inline void
+gc_init(void)
+{
+        if (!gc_flag) {
+                GC_INIT();
+                gc_flag = true;
+        }
+}
 
 
 extern rush_erno
 rush_mpool_alloc(void **bfr, size_t sz)
 {
-        if (!gc_init)
-                GC_INIT();
+RUSH_TRY:
+        rush_assert_handle(bfr);
+        rush_assert_range(sz);
 
+        gc_init();
         *bfr = GC_MALLOC(sz);
-        return *bfr != 0;
+        rush_assert_handle(*bfr);
+
+RUSH_CATCH:
+        rush_erno_set(RUSH_ERNO_MPOOL);
+
+RUSH_FINALLY:
+        return rush_erno_get();
 }
 
 
 extern rush_erno
 rush_mpool_realloc(void **bfr, size_t sz)
 {
-        if (!gc_init)
-                GC_INIT();
+RUSH_TRY:
+        rush_assert_handle(bfr);
+        rush_assert_range(sz);
 
+        gc_init();
         *bfr = GC_REALLOC(*bfr, sz);
-        return *bfr != 0;
+        rush_assert_handle(*bfr);
+
+RUSH_CATCH:
+        rush_erno_set(RUSH_ERNO_MPOOL);
+
+RUSH_FINALLY:
+        return rush_erno_get();
 }
 
